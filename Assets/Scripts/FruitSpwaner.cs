8e69cfcc46b1156fd375a0a98041f63cab6a1e64
ShaoYangZhou <sy_zhou@pku.edu.cn>
2018-05-17 01:04:59 +0800

    v1.0
    v1.0完成基础Demo

---
+++ Assets/Scripts/FruitSpwaner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpwaner : MonoBehaviour {
+
+    public GameObject[] fruitPrefab;
+    private Object[] ob;
+    private void Awake()
+    {
+        ob=Resources.LoadAll("FruitPrefab");
+        fruitPrefab = new GameObject[ob.Length];
+        for (int i = 0; i < ob.Length; i++)
+        {
+            fruitPrefab[i] = (GameObject)ob[i];
+            if (fruitPrefab[i].GetComponent<Fruit>() == null)
+            {
+                fruitPrefab[i].AddComponent<Fruit>();
+            }
+        }
+        if (fruitPrefab == null)
+        {
+            Debug.Log("FruitPrefab is Null!!!");
+        }
+    }
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(SpwanFruit());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    IEnumerator SpwanFruit()
+    {
+        while (true)
+        {
+            Vector3 pos = transform.position;
+            pos.x += Random.Range(-1f, 1f);
+            GameObject go=Instantiate(fruitPrefab[Random.Range(0, fruitPrefab.Length)], pos, Quaternion.Euler(-90f, 0f, 0f));
+            Rigidbody temp;
+            if (go.GetComponent<Rigidbody>() == null)
+            {
+                temp = go.AddComponent<Rigidbody>();
+            }
+            else
+            {
+                temp = go.GetComponent<Rigidbody>();
+            }
+            temp.useGravity = true;
+            temp.angularDrag = 0f;
+            float multiplier = 5f;
+            temp.velocity = (transform.forward+transform.up)*multiplier;
+            temp.angularVelocity=new Vector3(Random.Range(0f,12f),0f,0f);
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position,new Vector3(0.2f,0.2f,0.2f));
+    }
+}
---
+++ Assets/Scripts/SwordCutter.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+public class SwordCutter : MonoBehaviour {
+
+	public Material capMaterial;
+    public SteamVR_TrackedController controller;
+    public SteamVR_TrackedObject trackObj;
+
+	// Use this for initialization
+	//void Start () {
+
+		
+	//}
+	
+	//void Update(){
+
+	//	if(Input.GetMouseButtonDown(0)){
+	//		RaycastHit hit;
+
+	//		if(Physics.Raycast(transform.position, transform.forward, out hit)){
+
+	//			GameObject victim = hit.collider.gameObject;
+
+	//			GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+	//			if(!pieces[1].GetComponent<Rigidbody>())
+	//				pieces[1].AddComponent<Rigidbody>();
+
+	//			Destroy(pieces[1], 1);
+	//		}
+
+	//	}
+	//}
+
+	void OnDrawGizmosSelected() {
+
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
+		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.5f);
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+        {
+            pieces[1].AddComponent<Rigidbody>();
+            MeshCollider temp= pieces[1].AddComponent<MeshCollider>();
+            temp.convex = true;
+        }
+        StartCoroutine(LongVibration(.2f,.2f));
+        //Destroy(pieces[1], 1);
+
+    }
+
+    //SteamVR_Controller.Input([the index of the controller you want to vibrate]).TriggerHapticPulse([length in microseconds as ushort]);
+
+    IEnumerator LongVibration(float length, float strength)
+    {
+        for (float i = 0; i < length; i += Time.deltaTime)
+        {
+            SteamVR_Controller.Input((int)trackObj.index).TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+        yield return null;
+    }
+}
+}
